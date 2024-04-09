d313f27760b8e39a2f17d378cbc8a21c8cac3412
adobocoder <jcruz@adobo.dev>
2024-04-08 19:18:22 -0700

    create action method

--- COMP003B.SP2024.Lecture3/Controllers/StudentsController.cs
+++ COMP003B.SP2024.Lecture3/Controllers/StudentsController.cs
@@ -14,5 +14,33 @@ namespace COMP003B.SP2024.Lecture3.Controllers
         {
             return View(_students);
         }
+
+        // GET: Students/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Students/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Student student)
+        {
+            // TODO: add model state validation
+           if (ModelState.IsValid)
+            {
+                // TODO: check if student already exists
+                if (!_students.Any(s => s.Id == student.Id))
+                {
+                    // TODO: add student to list
+                    _students.Add(student);
+                }
+
+                // TODO: redirect back to index
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
     }
 }
--- COMP003B.SP2024.Lecture3/Models/Student.cs
+++ COMP003B.SP2024.Lecture3/Models/Student.cs
@@ -5,12 +5,14 @@ namespace COMP003B.SP2024.Lecture3.Models
 {
     public class Student
     {
+        // TODO: other c# built-in data types https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types
         // TODO: data in square brackets are data annotations
         [Required]
         // TODO: below is an example of a property
         public int Id { get; set; }
         [Required]
         [StringLength(100)]
+        [Display(Name="Full Name")]
         public string Name { get; set; }
         [Required]
         [Range(0,120)]
